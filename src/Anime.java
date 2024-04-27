606438c0d07928c905354ef8f2688fe0fec4894f
David Parra <leonpardoparrapardo23@gmail.com>
2024-04-27 09:42:15 -0500

    se le agrego nuevas cosas

--- src/Anime.java
+++ src/Anime.java
@@ -1,6 +1,27 @@
-public class Pelicula {
+public class Anime {
     String nombre;
     int fechaDeLanzamiento;
-    int duracionEnMinutos;
+    int capitulos;
+    int temporadas;
     boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalDeLasEvaluaciones;
+
+    void muestraFichaTecnica(){
+        System.out.println("El anime es: " + nombre);
+        System.out.println("Salio en el año: " + fechaDeLanzamiento);
+        System.out.println("Tiene: " + capitulos + " Capitulos");
+        System.out.println("Tiene: " + temporadas + " temporadas");
+    }
+
+//no necesita retornar nada
+    void evalua(double nota) {
+        sumaDeLasEvaluaciones += nota;
+        sumaDeLasEvaluaciones = sumaDeLasEvaluaciones + nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Principal {
+public class Principal {
+
+    public static void main(String[] args) {
+        Anime miAnime = new Anime();
+        miAnime.nombre = "Demon Slayer";
+        miAnime.fechaDeLanzamiento = 2019;
+        miAnime.capitulos = 26 ;
+        miAnime.temporadas = 4;
+
+        //Se ejecuta ya que en el Anime.java creamos un vod y se le agrego el sout (System.in.out)
+        miAnime.muestraFichaTecnica();
+        miAnime.evalua(8.5);
+        miAnime.evalua(9.0);
+
+        System.out.println(miAnime.sumaDeLasEvaluaciones);
+
+        Anime otroAnime = new Anime();
+        otroAnime.nombre = "Naruto";
+        otroAnime.fechaDeLanzamiento = 2002;
+        otroAnime.capitulos = 220;
+        otroAnime.temporadas = 9;
+
+        otroAnime.muestraFichaTecnica();
+    }
 }
